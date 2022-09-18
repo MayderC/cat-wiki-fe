c73f26ecce4edde0a8db882d737c4df372836eb7
Mayder C <maydercortes1@gmail.com>
2022-09-18 09:59:51 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+
+</script>
+
+<template>
+    <nav class="navbar flex-center-v">
+      <img class="img-logo" src="./assets/img/CatwikiLogo.svg" alt="">
+    </nav>
+  <RouterView />
+
+  <footer class="footer-main">
+
+  </footer>
+
+</template>
+
+<style>
+
+.flex-center-v{
+    display: flex;
+    align-items: center;
+  }
+  .flex-center-h{
+    display: flex;
+    justify-content: center;
+  }
+  .flex-center-b{
+    display: flex;
+    align-items: center;
+    align-items: center;
+  }
+  .navbar{
+    height: 99px;
+    padding: 0 96px;
+  }
+
+  .footer-main{
+    background-color: black;
+    color: aliceblue;
+    height: 99px;
+    border-radius: 42px 42px 0 0;
+    margin: 0 96px;
+  }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,51 @@
+<template>
+  <main>
+
+    <header>
+
+    </header>
+    <section class="section-top">
+
+    </section>
+    <section class="section-md">
+
+    </section>
+
+    <footer>
+
+    </footer>
+  </main>
+</template>
+<script setup lang="ts">
+
+</script>
+<style>
+
+  main{
+    padding: 0 96px;    
+  }
+  .img-logo{
+    height: 43px;
+  }
+  header{
+    background-image: url(../assets/img/HeroImagelg.png);
+    height: 538px;
+    background-size: cover;
+    border-radius: 42px 42px 0 0;
+    background-position: 0 0;
+    background-repeat: no-repeat;
+    width: 100%;
+  }
+
+  .section-top{
+    background-color: #E3E1DC;
+    height: 686px;
+    border-radius: 0 0 42px 42px;
+  }
+
+  .section-md{
+    height: 686px;
+  }
+
+
+</style>
